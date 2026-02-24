c8c0db7beb11a481574ec1fa41db95e9c86ef44d
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-24 20:40:05 -0300

    Create 2_operadores.php

---
+++ PHP/2_operadores.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = 10;
+$numero2 = 20;
+
+echo "Soma: " . ($numero1 + $numero2) . "<br>";
+echo "Subtração: " . ($numero1 - $numero2) . "<br>";
+echo "Multiplicação: " . ($numero1 * $numero2) . "<br>";
+echo "Divisão: " . ($numero1 / $numero2) . "<br>";
+echo "Potenciação: " . ($numero1 ** $numero2) . "<br>";
+?>
