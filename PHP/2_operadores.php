179656c01881716203eb793fb92c76255dbb42a9
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-17 21:33:51 -0300

    Update 2_operadores.php

--- PHP/2_operadores.php
+++ PHP/2_operadores.php
@@ -3,5 +3,7 @@ $numero1 = 10;
 $numero2 = 20;
 echo "Soma" .($numero1 + $numero2). "<br>";
 echo "Subtracao" .($numero1 - $numero2). "<br>";
-echo "Divisao" .($numero1 / $numero2). "br";
+echo "Multiplicacao" .($numero1 * $numero2). "<br>";
+echo "Divisao" .($numero1 / $numero2). "<br>";
+echo "Potenciação" .($numero1 ** $numero2). "<br";
 ?>
